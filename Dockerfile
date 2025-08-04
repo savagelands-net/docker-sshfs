bdebd7c3ffa0cdc42ae87258e76757a859417bb8
iain <iain@savagelands.net>
2025-08-04 10:50:33 +0100

    added ability to run as user

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM alpine:latest
 
-LABEL maintainer "https://github.com/gjyoung1974"
+LABEL maintainer "https://github.com/savagelands-net"
 
-ENV ROOT_PASSWORD root 
+ENV ROOT_PASSWORD root
 
 RUN apk --update add openssh \
-		&& sed -i s/#PermitRootLogin.*/PermitRootLogin\ yes/ /etc/ssh/sshd_config \
-		&& echo "root:${ROOT_PASSWORD}" | chpasswd \
-		&& rm -rf /var/cache/apk/* /tmp/* 
+    && sed -i s/#PermitRootLogin.*/PermitRootLogin\ yes/ /etc/ssh/sshd_config \
+    && echo "root:${ROOT_PASSWORD}" | chpasswd \
+    && rm -rf /var/cache/apk/* /tmp/*
 
 COPY entrypoint.sh /usr/local/bin/
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,24 @@
 ssh-keygen -A
 
 # check wether a random root-password is provided
-if [ ! -z ${ROOT_PASSWORD} ] && [ "${ROOT_PASSWORD}" != "root" ]; then
+if [ ! -z ${ROOT_PASSWORD} ] && [ -z ${ROOT_AUTHORIZED_KEY} ] && [ "${ROOT_PASSWORD}" != "root" ]; then
     echo "root:${ROOT_PASSWORD}" | chpasswd
+    mkdir -p /root/.ssh
+    touch /root/.ssh/authorized_keys
+    echo ${ROOT_AUTHORIZED_KEY} > /root/.ssh/authorized_keys
+    chmod 600 /root/.ssh/authorized_keys
 fi
 
-mkdir -p /root/.ssh
-touch /root/.ssh/authorized_keys
-echo ${ROOT_AUTHORIZED_KEY} > /root/.ssh/authorized_keys
-chmod 600 /root/.ssh/authorized_keys
+# check if user is provided
+if [ ! -z ${USER} ] && [ ! -z ${USER_PASSWORD} ] && [ ! -z ${USER_ID} ] && [ ! -z ${USER_GROUP_ID} ] && [ -z ${USER_AUTHORIZED_KEY} ]; then
+    useradd -m -u ${USER_ID} -g ${USER_GROUP_ID} -s /bin/bash ${USER}
+    echo "${USER}:${USER_PASSWORD}" | chpasswd
+    mkdir -p /home/${USER}/.ssh
+    touch /home/${USER}/.ssh/authorized_keys
+    echo ${USER_AUTHORIZED_KEY} > /home/${USER}/.ssh/authorized_keys
+    chown -R ${USER}:${USER} /home/${USER}/.ssh
+    chmod 600 /home/${USER}/.ssh
+fi
 
 # do not detach (-D), log to stderr (-e), passthrough other arguments
 exec /usr/sbin/sshd -D -e "$@"
